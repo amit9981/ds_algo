41dbb85227b85ed91537cd000dd6a48961a4acb5
amit <agupta8913@gmail.com>
2022-12-11 19:54:44 +0530

    filter api

---
+++ src/main/java/com/java/api/DistinctAndSets.java
@@ -0,0 +1,46 @@
+package com.java.api;
+
+//import org.junit.Test;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.java.beans.Car;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DistinctAndSets {
+    @Test
+    public void distinct() {
+
+        List<Integer> numbers = Arrays.asList(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+        List<Integer> distinct = numbers.stream().distinct().collect(Collectors.toList());
+        assertThat(distinct).hasSize(9);
+        System.out.println(distinct);
+    }
+
+    @Test
+    public void distinctWithSet() {
+        List<Integer> numbers = Arrays.asList(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+        Set<Integer> distinct = numbers.stream().collect(Collectors.toSet());
+        assertThat(distinct).hasSize(9);
+        System.out.println(distinct);
+    }
+
+    @Test
+    public  void findCarPrice() throws IOException {
+        MockData.getCars().stream().filter(p->p.getPrice()<10000).collect(Collectors.toList()).forEach(p-> System.out.println(p.getPrice()));
+      //  assertThat(selectedCars).hasSize(57);
+
+    }
+
+    @Test
+    public  void mapIterator() throws IOException {
+        Map<Integer, String> namesMap = new HashMap<>();
+        namesMap.put(1, "Larry");
+        namesMap.put(2, "Steve");
+        namesMap.put(3, "James");
+        namesMap.forEach((key,val)-> System.out.println(key+"  " +val));
+    }
+}
---
+++ src/main/java/com/java/api/MockData.java
@@ -0,0 +1,43 @@
+package com.java.api;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.java.beans.Car;
+import com.java.beans.Person;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.common.io.Resources;
+import org.apache.commons.io.IOUtils;
+
+public class MockData {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("hi");
+        System.out.println(getPeople());
+    }
+    public static List<Person> getPeople() throws IOException {
+        InputStream inputStream = Resources.getResource("people.json").openStream();
+        String json = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        Type listType = new TypeToken<ArrayList<Person>>() {
+        }.getType();
+        return new Gson().fromJson(json, List.class);
+    }
+
+    public static List<Car> getCars() throws IOException {
+        InputStream inputStream = Resources.getResource("cars.json").openStream();
+        String json = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        Type listType = new TypeToken<ArrayList<Car>>() {
+        }.getType();
+        return new Gson().fromJson(json, listType);
+    }
+
+}
