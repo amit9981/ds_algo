1dd3d20b3c3d321923117e5ae02234cc992725c7
amit <agupta8913@gmail.com>
2022-12-14 18:20:21 +0530

    filter api

--- src/main/java/com/java/api/Filtering.java
+++ src/main/java/com/java/api/Filtering.java
@@ -47,7 +47,8 @@ public class Filtering {
         System.out.println("using filter");
         Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
                 .forEach(n -> System.out.print(n + " "));
-
+        System.out.println("new");
+        Stream.of(1, 2, 6, 5).takeWhile(n -> n > 3).forEach(System.out::println);
         System.out.println();
         System.out.println("using take while");
         Stream.of(2, 4, 6, 8, 9, 10, 12).takeWhile(n -> n % 2 == 0)
