33a99b578a929b5c651b9a959ef1bef9da58bf3a
amit <agupta8913@gmail.com>
2023-01-02 11:51:07 +0530

    mapExample

--- src/main/java/com/java/api/UseOfJava8Map.java
+++ src/main/java/com/java/api/UseOfJava8Map.java
@@ -11,7 +11,16 @@ public class UseOfJava8Map {
     @Test
     public void MapUse() throws Exception {
         List<Car> cars = MockData.getCars();
-        System.out.println(cars.stream().map(n->n.getColor()).collect(Collectors.toList()));
+        System.out.println(cars.stream().map(n -> n.getColor()).collect(Collectors.toList()));
 
+        // to set value is list of custom object
+       /* List<Employee> l2=list.stream().map(t->{
+                    t.setAge(t.getAge()*2);
+                    return t;
+                }
+        ).collect(Collectors.toList());
+        */
+//or
+//fruits.forEach(fruit -> fruit.setName(fruit.getName() + "s"));
     }
 }
