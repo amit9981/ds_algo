ec40e7fd2d27294049331df6c0263acdb9a02ac5
amit <agupta8913@gmail.com>
2023-01-07 13:35:40 +0530

    'auto'

--- src/main/java/com/rnd/TerminalProcessMain.java
+++ src/main/java/com/rnd/TerminalProcessMain.java
@@ -24,13 +24,15 @@ public class TerminalProcessMain {
         System.out.println("Command: " + command);
         System.out.println("git add started : " + command.get(0));
         Process p1 = Runtime.getRuntime().exec(command.get(0), null, whereToRun);
+        boolean isFinished1 = p1.waitFor(50, TimeUnit.SECONDS);
         System.out.println("commit started : " + command.get(1));
         Process p2 = Runtime.getRuntime().exec(command.get(1), null, whereToRun);
+        boolean isFinished2 = p2.waitFor(50, TimeUnit.SECONDS);
         System.out.println("git push started : " + command.get(2));
         Process p3 = Runtime.getRuntime().exec(command.get(2), null, whereToRun);
         boolean isFinished3 = p3.waitFor(60, TimeUnit.SECONDS);
-        boolean isFinished1 = p1.waitFor(50, TimeUnit.SECONDS);
-        boolean isFinished2 = p2.waitFor(50, TimeUnit.SECONDS);
+
+
         if (isFinished1 && isFinished2 && isFinished3) {
             cleanUp(p1, p2, p3);
         }
