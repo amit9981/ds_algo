a5e5f0c4f9c7093247feb55f84ede5550bb814a1
amit <agupta8913@gmail.com>
2023-01-07 13:28:29 +0530

    'auto'

--- src/main/java/com/rnd/TerminalProcessMain.java
+++ src/main/java/com/rnd/TerminalProcessMain.java
@@ -11,98 +11,40 @@ public class TerminalProcessMain {
     public static void main(String[] args) throws Exception {
         // Where we want to execute
         File location = new File("/Users/amit/Desktop/project/new/ds_algo");
-        ArrayList al = new ArrayList();
-       // al.add("git status");
-        al.add("git add .");
-        al.add("git commit -a -m  'auto'");
-        al.add("git push");
+        ArrayList commands = new ArrayList();
+        commands.add("git add .");
+        commands.add("git commit -a -m  'auto'");
+        commands.add("git push");
 
-        runCommand(location, al); // for Mac(Linux based OS) users list files
-
-        // runCommand(location, "dir"); // For Windows users list files
+        runCommand(location, commands); // for Mac(Linux based OS) users list files
     }
 
     public static void runCommand(File whereToRun, List<String> command) throws Exception {
         System.out.println("Running in: " + whereToRun);
         System.out.println("Command: " + command);
-        Process process = Runtime.getRuntime().exec(command.get(0), null, whereToRun);
-        Process process1 = Runtime.getRuntime().exec(command.get(1), null, whereToRun);
-        Process process2 = Runtime.getRuntime().exec(command.get(2), null, whereToRun);
+        System.out.println("git add started : " + command.get(0));
+        Process p1 = Runtime.getRuntime().exec(command.get(0), null, whereToRun);
+        System.out.println("commit started : " + command.get(1));
+        Process p2 = Runtime.getRuntime().exec(command.get(1), null, whereToRun);
+        System.out.println("git push started : " + command.get(2));
+        Process p3 = Runtime.getRuntime().exec(command.get(2), null, whereToRun);
+        cleanUp(p1,p2,p3);
 
-        boolean isFinished = process.waitFor(50, TimeUnit.SECONDS);
-        boolean isFinished1 = process1.waitFor(50, TimeUnit.SECONDS);
-        boolean isFinished2 = process2.waitFor(30, TimeUnit.SECONDS);
-
-        if (isFinished && isFinished1 &&isFinished2) {
-            process.destroyForcibly();
-            process1.destroyForcibly();
-            process2.destroyForcibly();
-
-        }
-        /*ProcessBuilder builder = new ProcessBuilder();
-        builder.directory(whereToRun);
-        String s1 = "git commit ";
-        String s2 = "-a";
-        // String s3 = "'Auto commit test'";
-        String s4 = s1 + s2;
-
-        //for (int i = 0; i < command.size(); i++) {
-        builder.command("sh", "-c", "git add . ");
-        Process process = builder.start();
-       // process.destroy();
-
-        ProcessBuilder builder1 = new ProcessBuilder();
-        builder1.command("sh", "-c", "git commit -a -m 'test' ");
-        System.out.println("process start");
-        Process process1 = builder1.start();
-        process1.destroy();
-        System.out.println("process end");
-        ProcessBuilder builder2 = new ProcessBuilder();
-
-        builder2.command("sh", "-c", "push");
-        System.out.println("process start");
-        Process process2 = builder2.start();
-        process2.destroy();
-        //process.wait(1000, 100);
-        //process.destroyForcibly();
-        System.out.println("process destroy");*/
-        //}
-
-
-
-
-
-       /* if (isWindows) {
-            builder.command("cmd.exe", "/c", command);
-        } else {
-            builder.command("sh", "-c", command);
-        }*/
-
-
-      /*  OutputStream outputStream = process.getOutputStream();
-        InputStream inputStream = process.getInputStream();
-        InputStream errorStream = process.getErrorStream();
-
-        printStream(inputStream);
-        printStream(errorStream);
-
-        boolean isFinished = process.waitFor(30, TimeUnit.SECONDS);
-        outputStream.flush();
-        outputStream.close();
-
-        if (!isFinished) {
-            process.destroyForcibly();
-        }*/
     }
 
-    private static void printStream(InputStream inputStream) throws IOException {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-            }
+    private static void cleanUp(Process p1,Process p2,Process p3) throws InterruptedException {
+        System.out.println("process cleanUp Started : " );
 
+        boolean isFinished1 = p1.waitFor(50, TimeUnit.SECONDS);
+        boolean isFinished2 = p2.waitFor(50, TimeUnit.SECONDS);
+        boolean isFinished3 = p3.waitFor(30, TimeUnit.SECONDS);
+
+        if (isFinished1 && isFinished2 && isFinished3) {
+            p1.destroyForcibly();
+            p2.destroyForcibly();
+            p3.destroyForcibly();
         }
+        System.out.println("process cleanUp End : " );
     }
 
 }
