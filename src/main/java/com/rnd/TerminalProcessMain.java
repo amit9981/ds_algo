38522c2fed4ab57184333b8c095ce9b1e58464db
amit <agupta8913@gmail.com>
2023-01-07 10:18:10 +0530

    auto commit now

--- src/main/java/com/rnd/TerminalProcessMain.java
+++ src/main/java/com/rnd/TerminalProcessMain.java
@@ -1,37 +1,57 @@
 package com.rnd;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class TerminalProcessMain
-    {
-        private static boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
+public class TerminalProcessMain {
+    private static boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
 
-        public static void main (String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         // Where we want to execute
         File location = new File("/Users/amit/Desktop/project/new/ds_algo");
+        ArrayList al = new ArrayList();
+        al.add("git status");
+        al.add("git add .");
+        al.add("git commit -a -m  'auto commit now'");
+        al.add("git push");
 
-        runCommand(location, "git commit -a -m  'auto commit' "); // for Mac(Linux based OS) users list files
+        runCommand(location, al); // for Mac(Linux based OS) users list files
 
         // runCommand(location, "dir"); // For Windows users list files
     }
 
-        public static void runCommand (File whereToRun, String command) throws Exception {
+    public static void runCommand(File whereToRun, List<String> command) throws Exception {
         System.out.println("Running in: " + whereToRun);
         System.out.println("Command: " + command);
 
         ProcessBuilder builder = new ProcessBuilder();
         builder.directory(whereToRun);
 
-        if (isWindows) {
+        for (int i = 0; i <command.size() ; i++) {
+            builder.command("sh", "-c", "git add .");
+            builder.command("sh", "-c", "git commit -a -m  'auto commit now' ");
+            System.out.println("process start");
+            Process process = builder.start();
+            System.out.println("process end");
+            process.wait(1000,100);
+            //process.destroyForcibly();
+            System.out.println("process destroy");
+        }
+
+
+
+
+
+       /* if (isWindows) {
             builder.command("cmd.exe", "/c", command);
         } else {
             builder.command("sh", "-c", command);
-        }
+        }*/
 
-        Process process = builder.start();
 
-        OutputStream outputStream = process.getOutputStream();
+      /*  OutputStream outputStream = process.getOutputStream();
         InputStream inputStream = process.getInputStream();
         InputStream errorStream = process.getErrorStream();
 
@@ -44,10 +64,10 @@ public class TerminalProcessMain
 
         if (!isFinished) {
             process.destroyForcibly();
-        }
+        }*/
     }
 
-        private static void printStream (InputStream inputStream) throws IOException {
+    private static void printStream(InputStream inputStream) throws IOException {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
