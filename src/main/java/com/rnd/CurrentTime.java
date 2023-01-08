322f2d0931429e7cde6f14e1a1d6fa489c77bba2
amit <agupta8913@gmail.com>
2023-01-08 23:45:37 +0530

    'TerminalProcessMain_Changes_on_2023-01-08'

--- src/main/java/com/rnd/CurrentTime.java
+++ src/main/java/com/rnd/CurrentTime.java
@@ -4,16 +4,17 @@ import java.time.LocalDate;
 
 public class CurrentTime {
     public static void main(String[] args) {
-       new CurrentTime().generateGitMsg();
+        new CurrentTime().generateGitMsg();
     }
-    public String generateGitMsg(){
-        LocalDate today = LocalDate.now();
-        System.out.println("Current Date="+today);
-        String str=this.getClass().getName().substring(8,19)+" Changes on "+today;
 
-        System.out.println(str);
-        String gitCommandnew="git commit -a -m "+"'"+str+"'";
+    public String generateGitMsg() {
+        LocalDate today = LocalDate.now();
+        System.out.println("Current Date=" + today);
+        String str = this.getClass().getName().substring(8, 19) + " Changes on " + today;
+        System.out.println(System.getProperty("user.dir")+"/ds_algo");
+        String gitCommandnew = "git commit -a -m " + "'" + str + "'";
         System.out.println(gitCommandnew);
+
 
         return str;
     }
