67cdb9f1c50f7d78fac620ad5b54a7d0c544ce77
amit <agupta8913@gmail.com>
2023-01-17 11:05:47 +0530

    insertvalue_in_sorted_array

--- src/main/java/com/java/gfg/searching/SortedArray.java
+++ src/main/java/com/java/gfg/searching/SortedArray.java
@@ -53,76 +53,31 @@ public class SortedArray {
         }
         return -1;
     }
+
     @Test
     public void insertAtTheSortedArray() {
-        int val=13;
+        int val = 13;
         int[] array = testArray();
-        int n=6;
-        int capacity=array.length;
-        for (int i = n - 1; (i >= 0 ); i--) {
-            if(val>array[i]){
-                array[i]=val;
-                break;
-
-            }
-            array[i + 1] = array[i];
-            //array[i + 1] = val;
-
-
-        }
-        Arrays.stream(array).forEach(System.out::println);
-    }
-    public int[] testArray() {
-        int[] arr = new int[20];
-        arr[0] = 12;
-        arr[1] = 16;
-        arr[2] = 20;
-        arr[3] = 40;
-        arr[4] = 50;
-        arr[5] = 70;
-        return arr;
-    }
-
-    static int insertSorted(int arr[], int n, int key,
-                            int capacity)
-    {
-        // Cannot insert more elements if n is already
-        // more than or equal to capacity
-        if (n >= capacity)
-            return n;
-
-        int i;
-        for (i = n - 1; (i >= 0 && arr[i] > key); i--)
-            arr[i + 1] = arr[i];
-
-        arr[i + 1] = key;
-
-        return (n + 1);
-    }
-
-    /* Driver code */
-    public static void main(String[] args)
-    {
-        int arr[] = new int[20];
-        arr[0] = 12;
-        arr[1] = 16;
-        arr[2] = 20;
-        arr[3] = 40;
-        arr[4] = 50;
-        arr[5] = 70;
-        int capacity = arr.length;
         int n = 6;
-        int key = 26;
+        for (int i = n - 1; i >= 0; i--) {
+            array[i + 1] = array[i];
+            if (array[i] > val && array[i - 1] < val) {
+                array[i] = val;
+                break;
+             }
+            }
+            Arrays.stream(array).forEach(System.out::println);
+        }
 
-        System.out.print("\nBefore Insertion: ");
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
+        public int[] testArray() {
+            int[] arr = new int[20];
+            arr[0] = 12;
+            arr[1] = 16;
+            arr[2] = 20;
+            arr[3] = 40;
+            arr[4] = 50;
+            arr[5] = 70;
+            return arr;
+        }
 
-        // Function call
-        n = insertSorted(arr, n, key, capacity);
-
-        System.out.print("\nAfter Insertion: ");
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
     }
-}
