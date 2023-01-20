2d1a53219ad9a8034ac06e4a6f0b86606d334906
amit <agupta8913@gmail.com>
2023-01-21 00:03:03 +0530

    java

--- src/main/java/com/rnd/JavaConstant.java
+++ src/main/java/com/rnd/JavaConstant.java
@@ -4,7 +4,7 @@ public class JavaConstant {
     public static class  GitCommand{
         public static final String GIT_ADD ="git add .";
         public static final String GIT_COMMENTS= "Please enter comments";
-        public static final String GIT_COMMIT= "git commit -a -m";
+        public static final String GIT_COMMIT= "git commit -a -m ";
         public static final String GIT_PUSH= "Please enter comments";
         public static final String PROJECT_LOCATION ="/Users/amit/Desktop/project/new/ds_algo";
 
