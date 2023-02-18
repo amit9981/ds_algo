ded56b98ecf939707f3639e52287893ac246117e
amit <agupta8913@gmail.com>
2023-02-18 23:07:43 +0530

    ArrayRotate

---
+++ src/main/java/com/ds/arrays/RotateArray.java
@@ -0,0 +1,47 @@
+package com.ds.arrays;
+
+/*
+ * Given an array of integers arr[] of size N and an integer, the task is to rotate the array elements to the left by d positions.
+ *
+ * */
+/*
+Let us take arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2.
+
+First Step:
+        => Rotate to left by one position.
+        => arr[] = {2, 3, 4, 5, 6, 7, 1}
+
+Second Step:
+        => Rotate again to left by one position
+        => arr[] = {3, 4, 5, 6, 7, 1, 2}
+
+Rotation is done by 2 times.
+So the array becomes arr[] = {3, 4, 5, 6, 7, 1, 2}
+* */
+public class RotateArray {
+    public static void rotate(int arr[], int d, int n) {
+        int p = 1;
+
+        while (p<=d) {
+            int last = arr[0];
+            for (int i = 0; i < n - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[n-1]=last;
+            p++;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+        int N = arr.length;
+        // Rotate 2 times
+        int d = 2;
+
+        // Function call
+        rotate(arr, d, N);
+    }
+}
