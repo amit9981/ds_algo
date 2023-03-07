5a6aed2c8794905a3d80959b26e97837859db600
amit <agupta8913@gmail.com>
2023-03-07 23:58:33 +0530

    newfeature

---
+++ src/main/java/com/ds/list/ReverseLinkedListInPair.java
@@ -0,0 +1,18 @@
+package com.ds.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReverseLinkedListInPair {
+    public static void main(String[] args) {
+        List al=Stream.builder().add(1).add(2).add(3).add(4).add(5).add(6).build().collect(Collectors.toList());
+        System.out.println(al);
+        LinkedList ll=new LinkedList();
+        for (int i = 0; i < al.size(); i++) {
+
+        }
+    }
+}
--- src/main/java/com/java/jdk8/Demo.java
+++ src/main/java/com/java/jdk8/Demo.java
@@ -1,9 +1,31 @@
 package com.java.jdk8;
 
+import com.java.api.MockData;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String str = "86423279847";
         String search = "327";
-        System.out.println(str.indexOf(search));
+        //  System.out.println(str.indexOf(search));
+        int arr[] = {0, 1, 0, 1, 0, 0, 1};
+        MockData.getPeople().stream().filter(p -> p.getAge() > 98).sorted((c1,c2)->c1.getId().compareTo(c2.getId()))
+                .sorted((p1,p2)->p1.getFirstName().compareTo(p2.getFirstName()))
+                .collect(Collectors.toList()).forEach(System.out::println);
+        
+        ArrayList al = Stream.builder().add("hi").add("hello").add("jw").add("sdjh").add("sd").build()
+                .sorted(Collections.reverseOrder()).collect(Collectors.toCollection(ArrayList::new));
+        //Collections.sort(al);
+        System.out.println(al);
+
+        Arrays.stream(arr).sorted().forEach(System.out::println);
     }
+
 }
