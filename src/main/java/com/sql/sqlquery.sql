0ee492eba3923987495ceeca09286e8cf030e4f6
amit <agupta8913@gmail.com>
2023-04-05 00:39:33 +0530

    sql

--- src/main/java/com/sql/sqlquery.sql
+++ src/main/java/com/sql/sqlquery.sql
@@ -1,3 +1,5 @@
 SELECT DISTINCT City
 FROM Station
 WHERE UPPER(LEFT(CITY, 1)) IN ('A', 'E', 'I', 'O', 'U') AND LOWER(RIGHT(CITY,1))IN ('a','e','i','o','u');
+/*Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.*/
+select distinct CITY from STATION where ID%2 = 0
