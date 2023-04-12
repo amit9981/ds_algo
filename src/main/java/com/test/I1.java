1cc17e59b7a3d4e2b035601c30bdd44bb4a7c6af
amit <agupta8913@gmail.com>
2023-04-12 19:18:11 +0530

    test

--- src/main/java/com/test/I1.java
+++ src/main/java/com/test/I1.java
@@ -1,6 +1,7 @@
 package com.test;
-
+@FunctionalInterface
 public interface I1 {
+    void test();
       default void m1(){
           System.out.println("I1");
       }
@@ -10,7 +11,9 @@ public interface I1 {
     }
 
 }
-interface  I2{
+
+interface  I2 extends I1{
+    void t1();
     default void m1(){
         System.out.println("I2");
 
@@ -23,6 +26,18 @@ class A implements I1{
     void call(){
         m1();
     }
-    void callMe(){
+   /* void callMe(){
+    }
+    String callMe(){
+    }*/
+
+    @Override
+    public void test() {
+
+    }
+
+    @Override
+    public void m1() {
+        I1.super.m1();
     }
 }
