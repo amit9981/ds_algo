6a3fe18e0918a2870af2e369bf589db1d4aab9ab
amit <agupta8913@gmail.com>
2026-07-19 02:22:13 +0530

    Commit from Java

---
+++ src/main/java/com/job2026/GitAutomation.java
@@ -0,0 +1,24 @@
+package com.job2026;
+import java.io.IOException;
+
+public class GitAutomation {
+
+        public static void main(String[] args) throws InterruptedException, IOException {
+
+            String repoPath = "C:/Users/Amit/IdeaProjects/MyProject";
+            String username = "your-github-username";
+            String token = "your-personal-access-token";
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "git", "commit", "-m", "Initial Commit"
+            );
+
+            processBuilder.inheritIO();
+            Process process = processBuilder.start();
+            int exitCode = process.waitFor();
+
+            new ProcessBuilder("git", "add", ".").start();
+            new ProcessBuilder("git", "commit", "-m", "Commit from Java").start();
+            new ProcessBuilder("git", "push").start();
+            System.out.println("Exit Code: " + exitCode);
+        }
+}
