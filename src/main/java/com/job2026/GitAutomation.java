f2c1cbf77a031bcfb14ef3cab3418bb980e7ec84
amit <agupta8913@gmail.com>
2026-07-19 02:27:45 +0530

    test commit

--- src/main/java/com/job2026/GitAutomation.java
+++ src/main/java/com/job2026/GitAutomation.java
@@ -1,19 +1,15 @@
 package com.job2026;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class GitAutomation {
 
         public static void main(String[] args) throws InterruptedException, IOException {
-            /*ProcessBuilder processBuilder = new ProcessBuilder(
-                    "git", "commit", "-m", "Initial Commit"
-            );
-
-            processBuilder.inheritIO();
-            Process process = processBuilder.start();
-            int exitCode = process.waitFor();*/
-
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Please provide message");
+            String commitMsg=sc.nextLine();
             new ProcessBuilder("git", "add", ".").start();
-            new ProcessBuilder("git", "commit", "-m", "Commit from Java").start();
+            new ProcessBuilder("git", "commit", "-m", commitMsg).start();
             new ProcessBuilder("git", "push").start();
             System.out.println("Exit Code: " + "pushed");
         }
