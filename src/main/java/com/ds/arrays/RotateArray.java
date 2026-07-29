f0a2fa2189b2427edba6aae28b41e558d6c69c72
amit <agupta8913@gmail.com>
2026-07-29 11:01:37 +0530

    Commit Started in day8

--- src/main/java/com/ds/arrays/RotateArray.java
+++ src/main/java/com/ds/arrays/RotateArray.java
@@ -24,7 +24,11 @@ public class RotateArray {
 
         while (p<=d) {
             int last = arr[0];
-            if (n - 1 >= 0) System.arraycopy(arr, 1, arr, 0, n - 1);
+            //we can replace this with
+            // if (n - 1 >= 0) System.arraycopy(arr, 1, arr, 0, n - 1);
+            for (int i = 0; i < n - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
             arr[n-1]=last;
             p++;
         }
@@ -36,7 +40,6 @@ public class RotateArray {
         int p=1;
         while (p<=d){
             int temp=arr[length-1];
-            System.arraycopy(arr, 0, arr, 1, length - 1);
             for (int i = length-1;i>0; i--) {
                 arr[i]=arr[i-1];
             }
